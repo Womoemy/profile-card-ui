dc663dca86b8f6171fe99d2e5d518453fd6605fc
Womoemy <womoemiebiobowei@gmail.com>
2023-09-11 10:45:39 +0100

    fix errors

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+// display today's name
+const date = new Date();
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const currentDay = document.getElementById("currentDay");
+currentDay.innerHTML = weekdays[date.getDay()];
+
 // display current time
 function updateTime() {
     const currentTime = document.getElementById("currentTime");
     currentTime.innerHTML = new Date().getTime();
 }
-updateTime();
 setInterval(updateTime, 1000);
-
-// display today's name
-const options = { weekday: "long" };
-const currentDay = document.getElementById("currentDay");
-currentDay.innerHTML = new Intl.DateTimeFormat("en-NG", options).format(new Date());
